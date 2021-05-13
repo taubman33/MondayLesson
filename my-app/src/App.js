31a6a936734a07bc5387f4362c9d78da958a52c7
jataubman <jeremy.taubman@generalassemb.ly>
2021-05-13 12:33:50 -0400

    another branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,14 @@
 import React, {useState} from 'react'
 import './App.css';
 import Header from './components/Header.jsx'
-import Main from './components/Main.jsx'
-import Footer from './components/Footer.jsx'
-
 
 
 function App() {
   return (
     <div className="App">
     app
+
+    Appappapp
 
     </div>
   );
